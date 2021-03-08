db4c5ae515bb33e5ecdcc9a58bf8a37a72d00d01
Adam Czajka <czajka.adam147@gmail.com>
2021-03-08 13:37:27 +0100

    Initial

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace czajrowWebApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
